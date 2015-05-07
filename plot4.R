09e010c07d562b534c639991d303c82fb72af189
Geert Dobbels <dobbels.geert@gmail.com>
2015-05-07 23:08:23 +0200

    remove legend border in plot4

--- plot4.R
+++ plot4.R
@@ -13,6 +13,6 @@ plot(data$Datetime, data$Voltage, type = "l", xlab = "datetime", ylab = "Voltage
 plot(data$Datetime, data$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
 lines(data$Datetime, data$Sub_metering_2, type = "l", col = "red")
 lines(data$Datetime, data$Sub_metering_3, type = "l", col = "blue")
-legend("topright",c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lwd = c(1,1,1))
+legend("topright",c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty= "n", col = c("black", "red", "blue"), lwd = c(1,1,1))
 plot(data$Datetime, data$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power")
 dev.off()
